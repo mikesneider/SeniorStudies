e51b88516288155e88c1f0ccfdff304bcf556e8c
Mike <mikesneider@users.noreply.github.com>
2022-04-24 18:03:22 -0500

    Inicializando todo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SeniorC 
+{
+    //En C# 9.0 era obligatorio el: static void Main(string[] args)
+    class Program
+    {
+        public static void Main(){
+            string inicial = "Hola tu cachorro";
+            Console.WriteLine(inicial + (new TestAttribute().GetAnyText()));
+        }
+        //Console.WriteLine("Luego de C9");
+        
+    }   
+    //Hasta aquí resumen de una estructura C#, necesito el namespace,
+    // necesito la clase que sea del mismo nombre del archivo CS
+    // y a partir de C# no es necesario en Main
+
+}
+
---
+++ TestAttribute.cs
@@ -0,0 +1,14 @@
+//Aqui testing de Attribute
+//Primero armar el proyecto desde cero
+using System;
+
+namespace  SeniorC
+{
+    class TestAttribute
+    {
+        public string GetAnyText(){
+            return "Esto le tengo";
+        }
+
+    }
+}
