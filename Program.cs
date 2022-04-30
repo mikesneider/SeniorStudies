de2c2517d266f881887f2c01e410b268e0cc7bc0
Mike <mikesneider@users.noreply.github.com>
2022-04-30 18:11:40 -0500

    Atrribute pero sin cambios

--- Program.cs
+++ Program.cs
@@ -7,15 +7,25 @@ namespace SeniorC
     class Program
     {
         public static void Main(){
-            string inicial = "Hola tu cachorro";
-            Console.WriteLine(inicial + (new TestAttribute().GetAnyText()));
+            string? inicial = "Hola CSharp 11 \n ";
+            //como C# puedo poner el ? para que me acepte un valor NULL
+            Console.WriteLine(inicial );
+            new TestAttribute().GetAnyText(); //llamo a la función Main de GetAttribute
+
         }
-        //Console.WriteLine("Luego de C9");
         
+        public void NovedadAtributte()
+        {
+            //Primero, Attribute es para crear un tipo de objeto (type) particular utilizando herencia
+            //https://docs.microsoft.com/es-ES/dotnet/api/system.attribute?view=net-6.0
+            
+        }    
     }   
     //Hasta aquí resumen de una estructura C#, necesito el namespace,
     // necesito la clase que sea del mismo nombre del archivo CS
-    // y a partir de C# no es necesario en Main
+    // y a partir de C# no es necesario en Main PERO
+    // sin el namespace ni nada, esto sirve para hacer un console program para Azure o GutHubAction
+    //Y sin el namespace vale la pena para hacer el seguimiento. 
 
 }
 
--- TestAttribute.cs
+++ TestAttribute.cs
@@ -1,14 +1,75 @@
 //Aqui testing de Attribute
 //Primero armar el proyecto desde cero
 using System;
+using System.Reflection;
 
 namespace  SeniorC
 {
     class TestAttribute
     {
-        public string GetAnyText(){
-            return "Esto le tengo";
+        public void GetAnyText(){
+            Console.WriteLine("\nI am in Main");
+                AnimalTypeTestClass testClass = new AnimalTypeTestClass();
+                Type type = testClass.GetType();
+                // Iterate through all the methods of the class.
+                foreach(MethodInfo mInfo in type.GetMethods()) {
+                    // Iterate through all the Attributes for each method.
+                    foreach (Attribute attr in
+                        Attribute.GetCustomAttributes(mInfo)) {
+                        // Check for the AnimalType attribute.
+                        if (attr.GetType() == typeof(AnimalTypeAttribute))
+                            Console.WriteLine(
+                                "Method {0} has a pet {1} attribute.",
+                                mInfo.Name, ((AnimalTypeAttribute)attr).Pet);
+                    }
+                }
         }
 
+        // An enumeration of animals. Start at 1 (0 = uninitialized).
+        public enum Animal {
+            // Pets.
+            Dog = 1,
+            Cat,
+            Bird=2,
+        }
+
+        // A custom attribute to allow a target to have a pet.
+        public class AnimalTypeAttribute : Attribute {
+            // The constructor is called when the attribute is set.
+            public AnimalTypeAttribute(Animal pet) {
+                thePet = pet;
+            }
+
+            // Keep a variable internally ...
+            protected Animal thePet;
+
+            // .. and show a copy to the outside world.
+            //Lo siguiente puede ser también? Rta: no porque la variable ThePet no se muestra
+            //public Animal Pet {get; set;}
+            public Animal Pet {
+                get { return thePet; }
+                set { thePet = value; }
+            }
+        }
+
+        // A test class where each method has its own pet.
+        class AnimalTypeTestClass {
+            [AnimalType(Animal.Dog)]
+            public void DogMethod() {
+                Console.WriteLine("\nsay wow");
+            }
+
+            [AnimalType(Animal.Cat)]
+            public void CatMethod() {
+                Console.WriteLine("\nSay Miau");
+            }
+
+            [AnimalType(Animal.Bird)]
+            public void BirdMethod() {
+                Console.WriteLine("\nSay Twit");
+            }
+        }
+
+        
     }
 }
